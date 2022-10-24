b75257e2517a1cc38d62b3d02a7a94a417119b6a
Iqbalapredo <iqbalapreedo@gmail.com>
2022-10-24 23:25:51 +0700

    add auth page, landing page, flight detail page

--- src/views/auth/forgotPassword/index.js
+++ src/views/auth/forgotPassword/index.js
@@ -1,10 +1,38 @@
-import React from 'react'
-import styles from "../auth.module.css";
+import React from 'react';
+import styles from '../auth.module.css';
+import logo from '../../../assets/logo.svg';
 
 const ForgotPassword = () => {
   return (
-    <div>ForgotPassword</div>
-  )
-}
+    <>
+      <div className="row">
+        <div className={`${styles.column1} col-7`}></div>
+        <div className={`${styles.column2} col-5`}>
+          <img className={styles.logo1} src={logo} />
+          <h4 className={styles.textLogo1}>Ankasa</h4>
+          <div className={styles.textLogo2}>
+            <h3 className={styles.login}>Forgot Password</h3>
+          </div>
+          <div className={styles.loginBox}>
+            <form>
+              <div className={`${styles.userBox} ${styles.userBoxY}`}>
+                <input type="email" name="" required="" />
+                <label>Email</label>
+              </div>
+            </form>
+          </div>
+          <form className={`${styles.ButtonY} d-flex `} role="search">
+            <button className={`${styles.signUp} ${styles.signUp2}`} type="submit">
+              Send
+            </button>
+          </form>
+          <div className={styles.forgotY}>
+            <p className={`${styles.forgotP} ${styles.forgotP1}`}>You’ll get message soon on your email</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default ForgotPassword
+export default ForgotPassword;
